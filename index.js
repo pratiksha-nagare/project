c071a80ed49e9940d023ff3605775edcaca489f5
pratiksha-nagare <nagarebabasaheb4@gmail.com>
2024-09-21 16:45:26 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,117 +1,117 @@
-
-// function add(){
-//     var inputfield =document.querySelector(".box").value;
-//     var container = document.querySelector('.container');
-
-//      var h3Element = document.createElement('h3');
-//      h3Element .textContent=inputfield;
-
-//      container.appendChild(h3Element);
-
-//  }
-
-
-
-     var Object =[{
-            task:"hello"
-        },
-        {
-            task:"world"
-        },
-        {
-            task:"dream"
-        }
-        ,{
-            task:"Guider"
-        },
-        
-        {
-            task:"pratu"
-        },
-        {
-            task:"manu"
-        },
-        {
-            task:"nik"
-        }
-        ,{
-            task:"sonu"
-        },
-
-        {
-            task:"hello"
-        },
-        {
-            task:"world"
-        },
-        {
-            task:"dream"
-        }
-        ,{
-            task:"Guider"
-        },
-
-        {
-            task:"hello"
-        },
-        {
-            task:"world"
-        },
-        {
-            task:"dream"
-        }
-        ,{
-            task:"Guider"
-        },
-
-        {
-            task:"hello"
-        },
-        {
-            task:"world"
-        },
-        {
-            task:"dream"
-        }
-        ,{
-            task:"Guider"
-        },
-        {
-            task:"Guider"
-        },
-        {
-            task:"Guider"
-        },
+var constproperties =[
+    {
+        image:"",
+        name:"Name:Sunny Villa",
+        city:"City:New York",
+        price:"Price:500000",
+        discountedPrice:"DiscountedPrice:475000",
+        disc:"5%"
+    },
+    {
+        image:"",
+        name:"Name:Oceanfront Condo",
+        city:"City:Miami",
+        price:"Price:750000",
+        discountedPrice:"DiscountedPrice:700000",
+         disc:"6.67%"
+    },
+    {
+        image:"",
+        name:"Name:Mountain Retreat",
+        city:"City:Denver",
+        price:"Price:600000",
+        discountedPrice:"DiscountedPrice:570000",
+         disc:"4.44%"
+    },
+    {
+        image:"",
+        name:"Name:Urban Apartment",
+        city:"City:Chicago",
+        price:"Price:450000",
+        discountedPrice:"DiscountedPrice:430000",
+         disc:"5.56%"
+    }
+]
 
 
+constproperties.forEach(serve=>{
+    var box = document.createElement("div");
+box.className = "box";
 
-    ]
+var upperbox = document.createElement("div");
+upperbox.className = "upperbox";
+
+var lowerbox = document.createElement("div");
+lowerbox.className = "lowerbox";
+
+box.appendChild(upperbox);
+box.appendChild(lowerbox);
+
+var container = document.getElementById("details");
+container.appendChild(box);
+
+var nameHeading = document.createElement("h1");
+nameHeading.textContent = serve.name;
+
+var cityName = document.createElement("h2");
+cityName.textContent = serve.city;
+
+var villaPrice = document.createElement("h3");
+villaPrice.textContent = serve.price;
+
+var discount = document.createElement("h4");
+discount.textContent = serve.discountedPrice;
+
+var position = document.createElement("div");
+position.className= "position";
+
+var image = document.createElement("img");
+image.src=serve.image;
+
+var h1Element = document.createElement("h1");
+h1Element.textContent = serve.disc;
+
+position.appendChild(h1Element);
+box.appendChild(position);
+upperbox.appendChild(image);
+lowerbox.appendChild(nameHeading);
+lowerbox.appendChild(cityName);
+lowerbox.appendChild(villaPrice);
+lowerbox.appendChild(discount);
+})
+// var box = document.createElement("div");
+// box.className = "box";
+
+// var upperbox = document.createElement("div");
+// upperbox.className = "upperbox";
+
+// var lowerbox = document.createElement("div");
+// lowerbox.className = "lowerbox";
+
+// box.appendChild(upperbox);
+// box.appendChild(lowerbox);
+
+// var container = document.getElementById("details");
+// container.appendChild(box);
+
+// var nameHeading = document.createElement("h1");
+// nameHeading.textContent = constproperties.name;
+
+// var cityName = document.createElement("h2");
+// cityName.textContent = constproperties.city;
+
+// var villaPrice = document.createElement("h3");
+// villaPrice.textContent = constproperties.price;
+
+// var discount = document.createElement("h4");
+// discount.textContent = constproperties.discountedPrice;
 
 
-     Object.forEach(serve =>{
-    
-    var container=document.querySelector('.container');
-    var inputfield=document.querySelector('.inputfield');
+// var image = document.createElement("img");
+// image.src=serve.image;
 
-    
-
-    var h3Element = document.createElement("h3");
-    h3Element .textContent=serve.task;
-
-    
-    container.appendChild(h3Element);
-
-   })
-
-
-
-
-
-
-
-
-
-
-
-
-
+// upperbox.appendChild(image);
+// lowerbox.appendChild(nameHeading);
+// lowerbox.appendChild(cityName);
+// lowerbox.appendChild(villaPrice);
+// lowerbox.appendChild(discount);
