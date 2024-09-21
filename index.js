f39edd41a20050ac3e15e994729578d0d487e3e8
pratiksha-nagare <nagarebabasaheb4@gmail.com>
2024-09-21 16:38:05 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,117 @@
- function add(){
-    var inputfield =document.querySelector(".box").value;
-    var container = document.querySelector('.container');
 
-     var h3Element = document.createElement('h3');
-     h3Element .textContent=inputfield;
+// function add(){
+//     var inputfield =document.querySelector(".box").value;
+//     var container = document.querySelector('.container');
 
-     container.appendChild(h3Element);
+//      var h3Element = document.createElement('h3');
+//      h3Element .textContent=inputfield;
 
- }
+//      container.appendChild(h3Element);
+
+//  }
+
+
+
+     var Object =[{
+            task:"hello"
+        },
+        {
+            task:"world"
+        },
+        {
+            task:"dream"
+        }
+        ,{
+            task:"Guider"
+        },
+        
+        {
+            task:"pratu"
+        },
+        {
+            task:"manu"
+        },
+        {
+            task:"nik"
+        }
+        ,{
+            task:"sonu"
+        },
+
+        {
+            task:"hello"
+        },
+        {
+            task:"world"
+        },
+        {
+            task:"dream"
+        }
+        ,{
+            task:"Guider"
+        },
+
+        {
+            task:"hello"
+        },
+        {
+            task:"world"
+        },
+        {
+            task:"dream"
+        }
+        ,{
+            task:"Guider"
+        },
+
+        {
+            task:"hello"
+        },
+        {
+            task:"world"
+        },
+        {
+            task:"dream"
+        }
+        ,{
+            task:"Guider"
+        },
+        {
+            task:"Guider"
+        },
+        {
+            task:"Guider"
+        },
+
+
+
+    ]
+
+
+     Object.forEach(serve =>{
+    
+    var container=document.querySelector('.container');
+    var inputfield=document.querySelector('.inputfield');
+
+    
+
+    var h3Element = document.createElement("h3");
+    h3Element .textContent=serve.task;
+
+    
+    container.appendChild(h3Element);
+
+   })
+
+
+
+
+
+
+
+
+
+
+
+
+
